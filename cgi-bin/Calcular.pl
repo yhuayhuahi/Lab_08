d6e8a2187116c3fa59474fdd8df55da9b9716dbe
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2023-12-28 17:48:50 -0500

    Se completa parte del codigo html en el archivo de PERL

--- cgi-bin/Calcular.pl
+++ cgi-bin/Calcular.pl
@@ -7,3 +7,26 @@ use CGI;
 my $cgi = CGI -> new;
 my $expresion = $cgi -> param('Expresion');
 
+## Operaciones con expresiones regulares
+
+
+## Elaboracion del archivo html
+print $cgi -> header('text/html');
+print <<HTML;
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="autor" content="Yourdyy Yossimar Huayhua Hillpa">
+    <meta name="Descripción" content="Pagina web para hacer operaciones en linea.">
+    <link rel="shortcut icon" href="src/calculadora.ico" type="image/x-icon" />
+    <title>Calculadora Online</title>
+</head>
+<body>
+    
+</body>
+<html>
+
+
+HTML
